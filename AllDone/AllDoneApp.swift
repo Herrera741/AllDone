d918437d3e66d53ced8898a434e6807590d769b0
Sergio <sherrera725@gmail.com>
2023-06-04 23:46:02 -0700

    Initial Commit

---
+++ AllDone/AllDoneApp.swift
@@ -0,0 +1,17 @@
+//
+//  AllDoneApp.swift
+//  AllDone
+//
+//  Created by Sergio Herrera on 6/4/23.
+//
+
+import SwiftUI
+
+@main
+struct AllDoneApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
